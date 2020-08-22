9018fdb34c66200b1d05b5014c0e596d5b06dc0e
Karol Krysztof <kkrysztof@uniphar.pl>
2020-08-22 11:53:23 +0200

    KArol

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("Karol");
+        System.out.println(33);
+        System.out.println('K');
+    }
+}

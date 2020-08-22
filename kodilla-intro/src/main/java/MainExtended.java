ddd97751fe0aa289d97c5d3efb63106e74d69aa9
Karol Krysztof <kkrysztof@uniphar.pl>
2020-08-22 17:28:40 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "KArol";
+        int myAge = 34;
+        double height = 1.80;
+        char drivingLicenseCategory = 'B';
+        boolean isGoodProgrammer = true;
+        String isYourFirstBootcamp = "YES";
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(drivingLicenseCategory);
+        System.out.println(isGoodProgrammer);
+        System.out.println(isYourFirstBootcamp);
+    }
+}

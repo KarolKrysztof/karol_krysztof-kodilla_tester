81a0172af47faae1a3a902792ebba3bdc5ce93ee
Karol Krysztof <kkrysztof@uniphar.pl>
2020-09-10 22:32:56 +0200

    example-2.1-2.2 Programowanie obiektowe, klasy i obiekty w javie

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,10 +1,11 @@
 public class LeapYear {
     public static void main(String[] args) {
         int year = 2000;
-        if ((year%4 ==0 && year%100 !=0) || year%400 ==0) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             System.out.println("Is Leap Year");
         } else {
-                System.out.println("Is not Leap Year)");
+            System.out.println("Is not Leap Year)");
+
+                }
             }
         }
-    }

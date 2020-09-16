fe190b3910da962d2b3dee08fe48cb3e9440ed74
KarolKrysztof <69989543+KarolKrysztof@users.noreply.github.com>
2020-09-16 21:23:15 +0200

    Task 2.3 (#1)
    * example-2.1-2.2 Programowanie obiektowe, klasy i obiekty w javie
    
    * "task-2.3 Instrukcje Warunkowe"

---
+++ kodilla-intro/src/main/java/Notebook.java
@@ -0,0 +1,42 @@
+public class Notebook {
+    String weight;
+    int price;
+    int year;
+
+    public Notebook(String weight, int price, int year) {
+        this.weight = weight;
+        this.price = price;
+        this.year = year;
+    }
+
+    public void checkPrice() {
+        if (price < 600) {
+            System.out.println("This notebook is very cheap.");
+        } else if ((price >= 600) && (price <= 1000)) {
+            System.out.println("This notebook is good.");
+        } else if (price > 1000) {
+            System.out.println("This notebook is expensive.");
+        }
+    }
+
+    public void checkWeight() {
+        weight = weight.substring(0, weight.length() - 1);
+        int weight2 = Integer.parseInt(weight);
+
+        if (weight2 < 600) {
+            System.out.println("This notebook is light.");
+        } else if (weight2 > 2000) {
+            System.out.println("This notebook is heavy.");
+        } else {
+            System.out.println("This notebook is not very heavy");
+        }
+
+        if ((this.price > 2000) && (this.year > 2018)) {
+            System.out.println("This is new generation notebook");
+        } else if ((this.price >= 600) && (this.year < 2010)) {
+            System.out.println("This notebook is older generation and is cheap");
+        } else if ((this.price <= 1000) && (this.year >= 2015)) {
+            System.out.println("This notebook is mid-range");
+        }
+    }
+}
--- kodilla-intro/src/main/java/SimpleArray.java
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class SimpleArray {
+public class SimpleArray {
+
+    public static void main(String[] args) {
+        String[] carBrands = init();
+        printWithIndex(carBrands, 3);
+    }
+
+    private static String[] init() {
+        String[] carBrands = new String[5];
+        carBrands[0] = "Audi";
+        carBrands[1] = "Volvo";
+        carBrands[2] = "Mercedes";
+        carBrands[3] = "Kia";
+        carBrands[4] = "Toyota";
+        return carBrands;
+    }
+
+    private static void printWithIndex(String[] carBrands, int index) {
+        String carBrand = carBrands[index];
+        System.out.println(carBrand);
+    }
 }

4a77d7801fab01b1223b0f37e61a1090c5d12d86
Karol Krysztof <kkrysztof@uniphar.pl>
2020-09-16 22:45:16 +0200

    task-2.4 fix after review- dodanie obiektów, return Last Grade, return Average

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -1,34 +1,53 @@
 public class Grades {
-    public static void main(String[] args) {
-        private int[] grades;
-        private int size;
+
+    private int[] grades;
+    private int size;
 
     public Grades() {
-            this.grades = new int[10];
-            this.size = 0;
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (size == 10) {
+            return;
         }
+        grades[size] = value;
+        size++;
+    }
 
-        public void add(int value){
-            if (this.size == 10) {
-                return;
-            }
-            this.grades[this.size] = value;
-            this.size++;
+    public int returnLastGrade () {
+        return grades[size - 1];
+    }
 
-            public int returnLastGrade () {
-                return this.grades(this.size - 1);
-            }
-
-            public doble returnAverage () {
-                int sum = 0
-                for (int i = 0; i < grades.length; i++) {
-                    sum /= grades.length;
-                    return sum / this.size;
-                }
-            }
+    public double returnAverage () {
+        if (size == 0) {
+            return 0;
         }
+        double sum = 0;
+        for (int i = 0; i < size; i++) {
+           sum = sum + grades[i];
+        }
+        return sum / size;
+    }
+
+    public static void main(String[] args) {
+        Grades grades = new Grades();
+        grades.add(2);
+        grades.add(6);
+        grades.add(5);
+        grades.add(4);
+        grades.add(3);
+        grades.add(2);
+        grades.add(1);
+        grades.add(4);
+
+        System.out.println(grades.returnLastGrade());
+        System.out.println(grades.returnAverage());
+
     }
 }
+
 
 
 

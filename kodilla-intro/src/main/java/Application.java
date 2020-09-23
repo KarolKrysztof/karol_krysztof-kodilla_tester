6828e2c2274ec8f8a8385d416de5a02fa8f2309c
Karol Krysztof <kkrysztof@uniphar.pl>
2020-09-24 00:06:16 +0200

    task-2.6 fix after review

--- kodilla-intro/src/main/java/Application.java
+++ kodilla-intro/src/main/java/Application.java
@@ -1,13 +1,15 @@
 public class Application {
+
     public static void main(String[] args) {
 
-        Person adam = new Person("Adam", 40.5);
+        PersonValidation person = new PersonValidation("Adam", 40.5, 178);
 
-        if (adam.name != null) {
-            System.out.println("User is older than 30 and higher then 160cm");
-        } else {
-            System.out.println("User is exactly 30 or younger than 30 or he is exactly 160cm or shorter than 160cm");
-
+        if (person.hasName()) {
+            if (person.isOlderThan(30) && person.isHigherThan(160)) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is exactly 30 or younger than 30 or he is exactly 160cm or shorter than 160cm");
+            }
         }
     }
 }

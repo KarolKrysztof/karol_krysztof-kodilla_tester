8e6e69a39858628b7b5f96374b01346327ece28a
Karol Krysztof <kkrysztof@uniphar.pl>
2020-10-04 22:41:14 +0200

    jakiś błąd

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Triangle.java
@@ -0,0 +1,27 @@
+package com.kodilla.abstracts.homework;
+
+public class Triangle extends Shape {
+
+    double a;
+    double b;
+    double c;
+    double h;
+
+    public Triangle(double a, double b, double c, double h) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.h = h;
+    }
+
+    @Override
+    public double calculateArea() {
+        return (a * h) / 2;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return a + b + c;
+    }
+
+}

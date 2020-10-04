66fe024127c26ec49516521f8b79b6df54dbbe35
Karol Krysztof <kkrysztof@uniphar.pl>
2020-10-04 22:37:34 +0200

    task-3.5 wprowadzenie do testów jednostkowych- pierwszy test

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/baisc_asseration/Calculator.java
@@ -0,0 +1,18 @@
+package com.kodilla.abstracts.baisc_asseration;
+
+public class Calculator {
+
+    public int sum(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+
+    }
+
+    public int sqrt(int a) {
+        return a * a;
+    }
+
+}

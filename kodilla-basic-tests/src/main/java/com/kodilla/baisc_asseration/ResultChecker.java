061f81766ba14299c7c80e8a79c779089fe8ec9b
Karol Krysztof <kkrysztof@uniphar.pl>
2020-10-06 18:17:56 +0200

    task-3.6 podstawy JUnit testy jednostkowe dla kalkulatora

--- kodilla-basic-tests/src/main/java/com/kodilla/baisc_asseration/ResultChecker.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/baisc_asseration/ResultChecker.java
@@ -1,4 +1,4 @@
-package com.kodilla.abstracts.baisc_asseration;
+package com.kodilla.baisc_asseration;
 
 public class ResultChecker {
     public static boolean assertEquals(int expected, int actual) {

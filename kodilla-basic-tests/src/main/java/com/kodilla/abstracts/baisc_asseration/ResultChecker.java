85ed74d5ea850faac61bdf3ecaa226d71a549e44
KarolKrysztof <69989543+KarolKrysztof@users.noreply.github.com>
2020-10-06 22:20:12 +0200

    Task 3.5 (#10)
    * example-3.2 klasy abstrakcyjne
    
    * task-3.5 wprowadzenie do testów jednostkowych- pierwszy test

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/baisc_asseration/ResultChecker.java
@@ -0,0 +1,8 @@
+package com.kodilla.abstracts.baisc_asseration;
+
+public class ResultChecker {
+    public static boolean assertEquals(int expected, int actual) {
+
+        return expected == actual;
+    }
+}

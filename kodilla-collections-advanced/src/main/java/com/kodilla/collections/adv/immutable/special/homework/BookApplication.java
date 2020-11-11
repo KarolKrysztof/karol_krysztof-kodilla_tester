6ef8dbc7c7c170e0b043472bfd6f3ed6c1348c68
Karol Krysztof <kkrysztof@uniphar.pl>
2020-11-11 21:50:09 +0100

    task-5.4

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookApplication.java
@@ -2,11 +2,16 @@ package com.kodilla.collections.adv.immutable.special.homework;
 
 public class BookApplication {
     public static void main(String[] args) {
-        Book book = new BookManager().createBook("Rich Dad Poor Dad", "Robert Kiyosaki");
-        Book book1 = new BookManager().createBook("How to Win Friends and Influence People", "Dale Carnegie");
+        BookManager bookManager = new BookManager();
+        Book book =  bookManager.createBook("Rich Dad Poor Dad", "Robert Kiyosaki");
+        Book book1 = bookManager.createBook("How to Win Friends and Influence People", "Dale Carnegie");
+        Book book2 = bookManager.createBook("Rich Dad Poor Dad", "Robert Kiyosaki");
 
-        System.out.println(book == book1);
-
+        if (book == book2) {
+            System.out.println("Book Manager works fine");
+        } else {
+            System.out.println("Book Manage failed");
+        }
     }
 
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/special/homework/BookManager.java
@@ -1,41 +1,30 @@
 package com.kodilla.collections.adv.immutable.special.homework;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 public class BookManager {
-    private Book createBook;
+    private Set<Book> books = new HashSet<>();
 
     public Book createBook(String title, String author) {
-        this.createBook = createBook;
-        Set<Book> books = new HashSet<>();
-        books.add(new Book("Rich Dad Poor Dad", "Robert Kiyosaki"));
-        books.add(new Book("How to Win Friends and Influence People", "Dale Carnegie"));
-     return createBook;
+        Book book = find(title, author);
+        if (book != null) {
+            return book;
+        }
+
+        Book book1 = new Book(title, author);
+        books.add(book1);
+
+        return book1;
     }
 
-    public Book getCreateBook() {
-        return createBook;
+    private Book find(String title, String author) {
+        for (Book book : books) {
+            if (book.getTitle().equals(title) && book.getAuthor().equals(author)) {
+                return book;
+            }
+        }
+        return null;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BookManager that = (BookManager) o;
-        return Objects.equals(createBook, that.createBook);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(createBook);
-    }
-
-    @Override
-    public String toString() {
-        return "BookManager{" +
-                "createBook=" + createBook +
-                '}';
-    }
 }

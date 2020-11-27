a388cbbc614d7bf00d0504c9fa3459e28229c559
Karol Krysztof <kkrysztof@uniphar.pl>
2020-11-27 21:05:14 +0100

    task-6.4

---
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -0,0 +1,23 @@
+package com.kodilla.exception.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Warehouse {
+
+    private List<Order> orders = new ArrayList<>();
+    private Order order;
+
+    public List<Order> addOrder (Order order) {
+        orders.add(order);
+        return orders;
+    }
+
+    public Order getOrder (String number) throws OrderDoesntExistException {
+        return orders
+                .stream()
+                .filter(u -> u.getNumber().equals(number))
+                .findFirst()
+                .orElseThrow(OrderDoesntExistException:: new);
+    }
+}

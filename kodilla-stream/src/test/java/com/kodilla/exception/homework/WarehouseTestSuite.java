913a73d745e84ea84c5b4f7866e53c5dc6aa6bf8
Karol Krysztof <kkrysztof@uniphar.pl>
2020-12-04 19:58:16 +0100

    task-6.5

---
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -0,0 +1,35 @@
+package com.kodilla.exception.homework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class WarehouseTestSuite {
+
+    @Test
+    public void isGetOrderReturnCorrectNumber() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
+        Order order1 = new Order("1");
+        warehouse.addOrder(order1);
+        // when
+        Order order = warehouse.getOrder("1");
+        // then
+        assertEquals("1", order.getNumber());
+
+    }
+
+    @Test(expected = OrderDoesntExistException.class)
+    public void isGetOrderReturnException() throws OrderDoesntExistException {
+        // given
+        Warehouse warehouse = new Warehouse();
+        Order order1 = new Order("1");
+        warehouse.addOrder(order1);
+        // when
+        Order order = warehouse.getOrder("5");
+        //then
+        assertEquals("5", warehouse.getOrder("1"));
+    }
+
+}

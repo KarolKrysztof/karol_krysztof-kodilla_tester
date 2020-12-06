c5acc4956f1a089f786d99881b5df2562b99a25c
Karol Krysztof <kkrysztof@uniphar.pl>
2020-12-06 20:19:00 +0100

    example-7.1

---
+++ kodilla-advanced-tests/src/main/java/com/kodilla/parametrized_tests/StringValidator.java
@@ -0,0 +1,11 @@
+package com.kodilla.parametrized_tests;
+
+public class StringValidator {
+
+    public boolean isBlank(String text) {
+
+        return text == null || text.trim().isEmpty();
+
+    }
+
+}

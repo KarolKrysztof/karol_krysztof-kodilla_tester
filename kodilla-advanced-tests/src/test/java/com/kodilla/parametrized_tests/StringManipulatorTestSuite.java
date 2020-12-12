821bc9fd7f70da8667d82b80b7b7bb347ee61402
Karol Krysztof <kkrysztof@uniphar.pl>
2020-12-12 20:10:36 +0100

    example-7.3

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/StringManipulatorTestSuite.java
@@ -1,8 +1,12 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -22,7 +26,7 @@ public class StringManipulatorTestSuite {
 
     @ParameterizedTest
 
-    @CsvSource(value = {"test,4", " OtHEr ,5", "E V e n t,5", "null ,4", "A,1"})
+    @MethodSource(value = "com.kodilla.parametrized_tests.StringsSources#provideStringsForTestingLength")
 
     public void shouldCalculateStringLengthWithoutSpace(String input, int expected) {
 
@@ -38,6 +42,16 @@ public class StringManipulatorTestSuite {
 
         assertEquals(expected, manipulator.countNumberOfCommas(input));
 
+    }
+
+    private static Stream<Arguments> providesStringsForTestingLength() {
+        return Stream.of (
+                Arguments.of("test", 4),
+                Arguments.of("OtHEr", 5),
+                Arguments.of("E V e n t", 5),
+                Arguments.of("null ", 4),
+                Arguments.of("A", 1)
+        );
     }
 
 }
